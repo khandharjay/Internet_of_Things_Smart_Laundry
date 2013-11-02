287651142e630caeb592f56e56383d34b0397081
Jay Khandhar <jay.khandhar@gmail.com>
2013-11-02 06:59:14 -0600

    sensor and mqtt code integrated

--- mqtt_basic/mqtt_basic.ino
+++ mqtt_basic/mqtt_basic.ino
@@ -17,7 +17,13 @@
 
 byte mac[]    = {  0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0xED };
 byte server[] = { 10, 217, 2, 1 };
-byte ip[]     = { 192, 168, 0, 2 };
+//byte ip[]     = { 192, 168, 0, 2 };
+
+// these constants won't change:
+const int ledPin = 13;      // led connected to digital pin 13
+const int knockSensor = A0; // the piezo is connected to analog pin 0
+const int threshold = 15;  // threshold value to decide when the detected sound is a knock or not-Original 100
+int sensorReading = 0;      // variable to store the value read from the sensor pin
 
 void callback(char* topic, byte* payload, unsigned int length) {
   // handle message arrived
@@ -52,18 +58,67 @@ void setup()
   Serial.println();
   
   if (client.connect("arduinoClient")) {
-    client.publish("public/laundarypal","hello world-from Jay");
-    //client.subscribe("inTopic");
+    //client.publish("public/laundarypal","hello world-from Jay");
+    client.publish("public/laundarypal","\"info\":\"bootup\"");
+    //client.subscribe("inTopic"); 
   }
+  
+  
 }
 
 void loop()
 {
-  int i=0;
+  int count=0,loop_count=0;
   client.loop();
-  for(i=0;i<10;i++)
-  {
-    client.publish("public/laundarypal","hello world-from Jay");
-  }
+  
+  
+ sensorReading = analogRead(knockSensor);   
+ 
+ //Keep looping till sensor value is lower than the threshold 
+ 
+ while(sensorReading < threshold)
+ {
+    sensorReading = analogRead(knockSensor);   
+    //Serial.println(sensorReading,DEC);
+ }
+ 
+ //Vibration started, send Start message
+ client.publish("public/laundarypal","\"started\":true");
+ Serial.println("Started");
+ //Keep looping till sensor value is lower than the threshold 
+ do
+ {
+   count=0;
+   for(loop_count=20 ; loop_count>0; loop_count--)
+   {
+    sensorReading = analogRead(knockSensor);
+    if(sensorReading >= threshold)
+     {
+       count++;
+       //Serial.println(sensorReading,DEC);
+     }   
+    delay(100);
+   }
+ }while(count>=1);
+ 
+//Verify if vibration has stopped
+ do
+ {
+   count=0;
+   for(loop_count=20; loop_count>0; loop_count--)
+   {
+     sensorReading = analogRead(knockSensor);   
+     if(sensorReading == 0)
+      {
+        count++;
+        //Serial.println(sensorReading,DEC);
+      }
+     delay(100);
+   }
+ }while(count != 20);
+ 
+ //Vibration Stopped-Send Update
+ client.publish("public/laundarypal","\"started\":false");
+ Serial.println("Stopped"); 
 }
 
